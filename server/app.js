26dd836160e8cae643496e32e51ecbc55e3a51ef
Rami Gökhan Kıcı <gokhankici@gmail.com>
2016-12-08 18:19:25 -0800

    listening for POST request

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
-var express = require('express')
-var app = express()
+var express    = require('express')
+var bodyParser = require('body-parser')
+var multer     = require('multer')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var app    = express()
+var upload = multer() // for parsing multipart/form-data
+
+app.use(bodyParser.json())                         // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.post('/', upload.array(), function (req, res, next) {
+  res.json(req.body)
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
